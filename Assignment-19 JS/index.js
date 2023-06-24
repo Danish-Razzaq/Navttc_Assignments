aa7ae25793abb9b37d4689b10413991b46978bfe
Muhammad Danish <125806096+Danish-Razzaq@users.noreply.github.com>
2023-06-24 10:19:54 +0500

    script.js

--- Assignment-19 JS/index.js
+++ Assignment-19 JS/index.js
@@ -1,11 +1,12 @@
 // Use If...else statement of JavaScript to write a code to Display “Good Morning” or “Good Afternoon” according to current Time.
 
 
-let hours = 13;
-if(hours >= 6 && hours < 12){
-    console.log('Good morning!');
-} else if( hours >=12 && hours < 18 ) {
-    console.log('Good afternoon!');
+let hours = new Date();
+let current_time = hours.getHours();
+if (current_time >= 6 && current_time < 12) {
+  console.log("Good morning!");
+} else if (current_time >= 12 && current_time <= 18) {
+  console.log("Good afternoon!");
 } else {
-    console.log('Good neight!');
+  console.log("Good neight!");
 }
